5ef47d20c9b69e9511442ce8a84eff9aeed30355
Jason Rai <jrai@homex.com>
2020-09-04 17:25:16 -0400

    docs

--- lib/src/ui/master_detail_layout.dart
+++ lib/src/ui/master_detail_layout.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:multi_screen_layout/multi_screen_layout.dart';
 
+/// Very similar to [TwoPageLayout]. This layout has better support for having
+/// related, "deeper", content in the second page that would usually be accessed
+/// by navigating to a new page.
+///
+/// It's common to use this type of layout when you have a list of items that
+/// when tapped let you view a detailed view of the item. Email and instant
+/// messaging apps are examples of this.
+///
+/// On a single screen device, or when the app is only running on a single
+/// screen, [master] will display first. When [isSelected] is true, [detail] is
+/// displayed as a new page on top of [master], similar to using `Navigator.push`.
+///
+/// When spanned across 2 screens, both [master] and [detail] display at the
+/// same time and no navigation occurs. Even when [isSelected] is false.
+///
+/// `MasterDetailLayout` also handles switching between spanned and non-spanned
+/// modes appropriately, so the UI will be the same if you select and then span,
+/// or span and then select.
 class MasterDetailLayout extends StatelessWidget {
   final Widget master;
   final Widget detail;
